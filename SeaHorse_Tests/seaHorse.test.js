b9714e7931ee529069b324fcec6cb5980f215224
konstantin <konstantin@encorp.io>
2024-08-06 16:22:37 +0300

    Optimize test run time and readability

--- SeaHorse_Tests/seaHorse.test.js
+++ SeaHorse_Tests/seaHorse.test.js
@@ -21,63 +21,45 @@ describe('SeaHorse endpoint tests', () => {
             "hourlyData": expect.arrayContaining(Array(0))
         };
 
+    // we need to make the request only once, so to optimize run time I moved it here.
+    let res;
+    let responseJSON;
+    beforeAll(async () => {
+        res = await fetch(baseUrl);
+        responseJSON = await res.json();
+    });
 
-    it('should return 200 OK', async () => {
-        let res = await fetch(baseUrl);
-        let responseStatus = await res.status;
-        let responseStatusText = await res.statusText;
-        
+    it('should return 200 OK', () => {
+        let responseStatus = res.status;
+        let responseStatusText = res.statusText;
         expect(responseStatus).toEqual(200);
         expect(responseStatusText).toEqual('OK');
     });
 
-    it('should contain a day object', async () => {
-        
-        let res = await fetch(baseUrl);
-        let responseJSON = await res.json();
-
-        expect(responseJSON).toHaveLength(224);
+    it('should contain a day object', () => {
         
         expect(responseJSON[0]).toMatchObject(dayStruct);
     });
 
-    it('should contain a hour object', async () => {
-        let res = await fetch(baseUrl);
-        let responseJSON = await res.json();
+    it('should return 224 days', () => {
+       
+        expect(responseJSON).toHaveLength(224);
         
-        let hourlyDataStruct = 
-            {
-                "data": {
-                    "eur": expect.any(Number),
-                    "bgn": expect.any(Number),
-                    "volume": expect.any(Number)
-                },
-                "time": expect.stringMatching(/^(0[0-9]|1[0-9]|2[0-4]):00:00$/),
-                "_id": expect.any(String)
-            };
+    });
 
+    it('should contain a hour object', () => {
+        
         expect(responseJSON[0].hourlyData[0]).toMatchObject(hourlyDataStruct);
     });
 
-    it('should check each day object', async () => {
-        let res = await fetch(baseUrl);
-        let responseJSON = await res.json();
+    it('should check each day object', () => {
         
-        let dayStruct = 
-            {
-                "_id": expect.any(String),
-                // The date regex checks it's a valid date within this year. This checks for anomalies in the date.
-                "date": expect.stringMatching(/^(0[1-9]|[12]\d|3[01])\.(0[1-9]|1[0-2])\.(202[0-4])$/),
-                "hourlyData": expect.arrayContaining(Array(0))
-            };
         responseJSON.forEach(day =>{
             expect(day).toMatchObject(dayStruct);
         });        
     });
 
     it('should check each hour in each day object', async () => {
-        let res = await fetch(baseUrl);
-        let responseJSON = await res.json();
         
         // iterate through each day and validate the structure of the object
         responseJSON.forEach(day =>{
